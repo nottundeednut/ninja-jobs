aa86503c61b205276fe317d57c49f1a3255a2993
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2023-02-15 20:43:50 +0100

    router done

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Jobs from "../views//jobs/Jobs.vue";
+import JobDetails from "../views//jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  }
-]
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
+    props: true,
+  },
+  // redirect
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  // 404 catchall
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
